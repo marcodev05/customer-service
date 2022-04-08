e3c8c89be4ceb17aa85de18db2e973e24e1d71e9
maminiaina marco <maminiainamarco@gmail.com>
2022-04-09 01:00:41 +0300

    fichier de deploiement

--- src/main/java/com/tsk/customerservice/CustomerServiceApplication.java
+++ src/main/java/com/tsk/customerservice/CustomerServiceApplication.java
@@ -3,13 +3,11 @@ package com.tsk.customerservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.tsk.customerservice.services.CustomerService;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
